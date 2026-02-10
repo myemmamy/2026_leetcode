b3d144038369fb968c0e9b1eac108bc17d0c2ffb
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-02-10 13:45:42 -0800

    Update 18_4Sum.py

--- 18_4Sum.py
+++ 18_4Sum.py
@@ -53,4 +53,38 @@ class Solution:
                     else:
                         right -= 1
         return res
+
+#20260210 change way to detect dup
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1,len(nums)-2):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue 
+                t1 = nums[i] + nums[j] 
+                t2 = target - t1
+                left = j+1
+                right = len(nums)-1
+                while left < right:
+                    if nums[left] + nums[right] == t2:
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right+1]:
+                            right -= 1
+                    elif nums[left] + nums[right] < t2:
+                        left += 1
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1                        
+                    else:
+                        right -= 1
+                        while left < right and nums[right] == nums[right+1]:
+                            right -= 1                   
+        return res
             
