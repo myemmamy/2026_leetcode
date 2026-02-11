82980fc31974cbccde0475f447743221c31f4273
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-02-10 17:56:30 -0800

    Update 15_3Sum.py

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -62,8 +62,12 @@ class Solution:
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
                 elif nums[left] + nums[right] < target:
-                    left += 1                
+                    left += 1    
+                    while left < right and nums[left] == nums[left -1]:
+                        left += 1
                 else:
                     right -= 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1                 
         return res  
 
