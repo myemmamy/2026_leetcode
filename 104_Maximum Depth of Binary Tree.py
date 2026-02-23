0ab691b22efe623d0b185c5883cd1a0473de2dd1
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-02-23 12:32:12 -0800

    Create 104_Maximum Depth of Binary Tree.py

---
+++ 104_Maximum Depth of Binary Tree.py
@@ -0,0 +1,40 @@
+DFS:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            return max(dfs(node.left),dfs(node.right)) + 1
+        return dfs(root)
+
+
+BFS:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        dep = 0
+        q = deque()
+        if not root:
+            return 0
+        q.append(root)
+        while q:
+            l = len(q)
+            dep += 1
+            for i in range(l):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return dep
