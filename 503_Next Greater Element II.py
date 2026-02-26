9f25dabb03c1ec399ac1792b79f465bac99c402f
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-02-26 11:30:46 -0800

    Create 503_Next Greater Element II.py

---
+++ 503_Next Greater Element II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [-1] * n
+        stack = []
+        for i in range(2*n):
+            while stack and nums[i%n] > nums[stack[-1]]:
+                p = stack.pop()
+                ans[p] = nums[i%n]
+            stack.append(i%n)
+        return ans
