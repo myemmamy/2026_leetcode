a6ce2fe3a95a94da75ed0c224b574dc2d32ac98c
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-02-27 22:12:07 -0800

    Update 735_Asteroid Collision.py

--- 735_Asteroid Collision.py
+++ 735_Asteroid Collision.py
@@ -3,35 +3,22 @@ class Solution:
         stack = []
         i = 0
         for i in range(len(asteroids)):
-            tag = 0
-            if asteroids[i] > 0:
-                stack.append(asteroids[i])
-            else:
-                if not stack:
-                    stack.append(asteroids[i])
-                    continue
-                if stack[-1] < 0:
-                    stack.append(asteroids[i])
-                elif 0 < stack[-1] < abs(asteroids[i]): 
-                    while stack:
-                        if stack[-1] < 0:
-                            break
-                        if stack[-1] < abs(asteroids[i]):
-                            stack.pop()
-                        elif stack[-1] == abs(asteroids[i]):
-                            stack.pop()
-                            tag = 1
-                            break
-                        else:
-                            tag = 1
-                            break
-                    if not tag :
-                        stack.append(asteroids[i])
-                elif stack[-1] == abs(asteroids[i]):
-                    stack.pop()
-                elif stack[-1] > abs(asteroids[i]):
-                    pass
-        return stack
-                    
-                
-                    
+            c =  asteroids[i]
+            if not stack or c > 0 or (stack[-1] < 0 and c < 0):
+                stack.append(c)
+            elif stack[-1] > 0 and c < 0:
+                active = True
+                while stack and active and stack[-1] > 0:
+                    if stack[-1] < abs(c):
+                        stack.pop()
+                    elif stack[-1] == abs(c):
+                        stack.pop()
+                        active = False
+                        break
+                    else:
+                        active = False
+                        break
+                if active:
+                    stack.append(c)
+
+        return stack    
