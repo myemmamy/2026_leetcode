069d2d2a61c2313941b23fd16c83001abe6cded3
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-02-28 13:25:50 -0800

    Create 150_Evaluate Reverse Polish Notation.py

---
+++ 150_Evaluate Reverse Polish Notation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in range(len(tokens)):
+            if tokens[i].isdigit():
+                stack.append(int(tokens[i]))
+            elif len(tokens[i]) > 1 and tokens[i][0] == '-':
+                stack.append(-int(tokens[i][1:]))
+            elif tokens[i] in '+-*/':
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+                if tokens[i] == '+':
+                    stack.append(operand1+operand2)
+                elif tokens[i] == '-':
+                    stack.append(operand2-operand1)
+                elif tokens[i] == '*':
+                    stack.append(operand1*operand2)
+                else:
+                    stack.append(int(operand2/operand1))
+        return stack[-1]
