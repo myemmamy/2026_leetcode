fec23b1e34f645c39a5861790ffa3bbeae35b103
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-03-07 13:44:49 -0800

    Update 100_Same Tree.py

--- 100_Same Tree.py
+++ 100_Same Tree.py
@@ -42,16 +42,15 @@ The number of nodes in both trees is in the range [0, 100].
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         def helper(n1,n2):
-            if n1 and n2:
-                if n1.val != n2.val:
-                    return False
-                else:
-                    return helper(n1.left,n2.left) and helper(n1.right, n2.right) 
-            elif not n1 and not n2:
-                return True
-            else:
+            if (not n1 and n2) or (n1 and not n2):
                 return False
+            if not n1 and not n2:
+                return True
+            if n1 and n2 and n1.val != n2.val:
+                return False
+            return helper(n1.left,n2.left) and helper(n1.right,n2.right)
         return helper(p,q)
+        
                 
 
   
