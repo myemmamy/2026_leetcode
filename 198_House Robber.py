d1ced647cb369b4f66a78e569c737ab938689adc
myemmamy <47264178+myemmamy@users.noreply.github.com>
2026-03-15 14:00:30 -0700

    Create 198_House Robber.py

---
+++ 198_House Robber.py
@@ -0,0 +1,9 @@
+#dp 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0]*(n+1)
+        dp[1] = nums[0]
+        for i in range(2,len(nums)+1):
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i-1])
+        return dp[n]
